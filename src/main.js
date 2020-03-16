b6fa05b67b85ada9a45e0575af885c6a31fd50a1
lmy <1751258687@qq.com>
2020-03-17 02:56:14 +0800

    完成基本项目源码目录设计和设计vue用户代码片段

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App) // 函数返回组件的实例化标签<App />
 }).$mount('#app')
