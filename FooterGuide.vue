0e1899ac0220cd1e23a8949e2f4a251aa9583964
lmy <1751258687@qq.com>
2020-03-31 18:38:24 +0800

    初步完成FooterGuide布局

--- FooterGuide.vue
+++ FooterGuide.vue
@@ -1,29 +1,29 @@
 <template>
-  <footer class="footer-guide border-1px">
-    <span class="guide-item">
-      <span>
+  <footer class="footer_guide border-1px">
+    <a href="javascript:;" class="guide_item on">
+      <span class="item_icon">
         <i class="iconfont icon-waimai"></i>
       </span>
       <span>外卖</span>
-    </span>
-    <span class="guide-item">
-      <span>
+    </a>
+    <a href="javascript:;" class="guide_item">
+      <span class="item_icon">
         <i class="iconfont icon-search"></i>
       </span>
       <span>搜索</span>
-    </span>
-    <span class="guide-item">
-      <span>
+    </a>
+    <a href="javascript:;" class="guide_item">
+      <span class="item_icon">
         <i class="iconfont icon-dingdan"></i>
       </span>
       <span>订单</span>
-    </span>
-    <span class="guide-item">
-      <span>
+    </a>
+    <a href="javascript:;" class="guide_item">
+      <span class="item_icon">
         <i class="iconfont icon-geren"></i>
       </span>
       <span>我的</span>
-    </span>
+    </a>
   </footer>
 </template>
 
@@ -34,7 +34,9 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
-  .footer-guide
+  @import '~@/assets/styles/mixins.styl'
+  .footer_guide
+    top-border-1px(#e4e4e4)
     position fixed
     z-index 100
     left 0
@@ -44,16 +46,16 @@
     width 100%
     height 50px
     display flex
-    &::before
-      border-top 1px solid #e4e4e4
-    .guide-item
+    .guide_item
+      display flex
       flex 1
       text-align center
-      display flex
       flex-direction column
-      justify-content center
+      align-items center
       margin 5px
       color #999999
+      &.on
+        color #02a774
       span
         font-size 12px
         margin-top 2px
--- postcss.config.js
+++ postcss.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     "postcss-cssnext": {},
     "postcss-px-to-viewport": {
-      viewportWidth: 750,     // (Number) The width of the viewport. 视窗的宽度，对应的是我们设计稿的宽度，一般是750
+      viewportWidth: 375,     // (Number) The width of the viewport. 视窗的宽度，对应的是我们设计稿的宽度，一般是750
       viewportHeight: 1334,   // (Number) The height of the viewport. 视窗的高度，根据750设备的宽度来指定，一般指定1334，也可以不配置
       unitPrecision: 3,       // (Number) The decimal numbers to allow the REM units to grow to. 指定`px`转换为视窗单位值的小数位数（很多时候无法整除）
       viewportUnit: 'vw',     // (String) Expected units. 指定需要转换成的视窗单位，建议使用vw
