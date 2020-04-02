65bf0d224eda6dbdc2744b263dde01cf01314a3a
lmy <1751258687@qq.com>
2020-04-03 01:12:03 +0800

    解决引入使用fastclick插件库后在苹果浏览器上双击会报错的bug;  解决引入使用fastclick插件库后在苹果浏览器上输入框(包括input和textarea)点击无响应(响应难、响应慢)问题的bug，即会产生输入框(包括input和textarea)点击无法获取焦点问题(或获取焦点难、获取焦点慢的问题)，只有双击或者长按的时候才能使input或textarea输入框获取到焦点的bug

--- src/main.js
+++ src/main.js
@@ -8,6 +8,18 @@ import './assets/styles/reset.css'
 Vue.config.productionTip = false
 
 
+/* 解决引入使用fastclick插件库后在苹果浏览器上输入框(包括input和textarea)点击无响应(响应难、响应慢)问题的bug，即会产生输入框(包括input和textarea)点击无法获取焦点问题(或获取焦点难、获取焦点慢的问题)，只有双击或者长按的时候才能使input或textarea输入框获取到焦点的bug */
+FastClick.prototype.focus = function(targetElement) {
+  let length
+  const isIOS = !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/) // 去获取当前系统是否为ios,继续执行判断
+  if ( isIOS && targetElement.setSelectionRange && targetElement.type.indexOf('date') !== 0 && targetElement.type !== 'time' && targetElement.type !== 'month') {
+    length = targetElement.value.length
+    targetElement.focus()
+    targetElement.setSelectionRange(length, length)
+  } else {
+    targetElement.focus()
+  } 
+}
 // 绑定fastclick到document.body上，解决移动端浏览器上点击(click)响应延时0.3s问题，它消除了在移动浏览器上物理点击和触发点击事件之间的300毫秒延迟(为什么会有300ms延迟? 这样做的原因是，浏览器正在等待查看您是否正在实际执行双击)。但到2015年底，大多数移动浏览器——尤其是Chrome和Safari——不再有300毫秒的触摸延迟，因此fastclick在新浏览器上没有任何好处，并有可能给你的应用程序带来bug。仔细考虑你是否真的需要使用它。
 FastClick.attach(document.body)
 
