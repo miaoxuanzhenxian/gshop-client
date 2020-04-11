8b158115256eef03cce9e53ceadff39c877b7b4f
lmy <1751258687@qq.com>
2020-04-11 21:26:08 +0800

    完成MSite组件的静态页面

--- src/pages/MSite/MSite.vue
+++ src/pages/MSite/MSite.vue
@@ -160,11 +160,149 @@
                     <div class="sales-section">月售106单</div>
                   </section>
                   <section class="info-brief-right">
-                    <span class="needed-time">30分钟</span>
+                    <span>30分钟</span>
                     <span class="shop-distance">443m</span>
                   </section>
                 </section>
-                <section class="shop-delivery">
+                <section class="shop-delivery clearfix">
+                  <section class="delivery-left">
+                    <div class="delivery-msg">
+                      <span>¥20起送</span>
+                      <span class="segmentation">/</span>
+                      <span>配送费约¥5</span>
+                    </div>
+                  </section>
+                  <section class="delivery-right">
+                    <span class="delivery-style">硅谷专送</span>
+                  </section>
+                </section>
+              </div>
+            </a>
+          </li>
+          <li class="shop-li border-1px">
+            <a class="shop-li-a clearfix">
+              <div class="shop-left">
+                <img class="shop-img" src="./images/shop/2.jpg" alt="">
+              </div>
+              <div class="shop-right">
+                <section class="shop-info-header clearfix">
+                  <h4 class="shop-title ellipsis">锄禾日当初，汗滴禾下土</h4>
+                  <ul class="info-header-ul">
+                    <li class="supports">保</li>
+                    <li class="supports">准</li>
+                    <li class="supports">票</li>
+                  </ul>
+                </section>
+                <section class="shop-info-brief clearfix">
+                  <section class="info-brief-left">
+                    <div class="star star-24">
+                      <span class="star-item on"></span>
+                      <span class="star-item on"></span>
+                      <span class="star-item on"></span>
+                      <span class="star-item on"></span>
+                      <span class="star-item off"></span>
+                    </div>
+                    <div class="rating-section">4.1</div>
+                    <div class="sales-section">月售106单</div>
+                  </section>
+                  <section class="info-brief-right">
+                    <span>30分钟</span>
+                    <span class="shop-distance">443m</span>
+                  </section>
+                </section>
+                <section class="shop-delivery clearfix">
+                  <section class="delivery-left">
+                    <div class="delivery-msg">
+                      <span>¥20起送</span>
+                      <span class="segmentation">/</span>
+                      <span>配送费约¥5</span>
+                    </div>
+                  </section>
+                  <section class="delivery-right">
+                    <span class="delivery-style">硅谷专送</span>
+                  </section>
+                </section>
+              </div>
+            </a>
+          </li>
+          <li class="shop-li border-1px">
+            <a class="shop-li-a clearfix">
+              <div class="shop-left">
+                <img class="shop-img" src="./images/shop/3.jpg" alt="">
+              </div>
+              <div class="shop-right">
+                <section class="shop-info-header clearfix">
+                  <h4 class="shop-title ellipsis">锄禾日当初，汗滴禾下土</h4>
+                  <ul class="info-header-ul">
+                    <li class="supports">保</li>
+                    <li class="supports">准</li>
+                    <li class="supports">票</li>
+                  </ul>
+                </section>
+                <section class="shop-info-brief clearfix">
+                  <section class="info-brief-left">
+                    <div class="star star-24">
+                      <span class="star-item on"></span>
+                      <span class="star-item on"></span>
+                      <span class="star-item on"></span>
+                      <span class="star-item off"></span>
+                      <span class="star-item off"></span>
+                    </div>
+                    <div class="rating-section">3.2</div>
+                    <div class="sales-section">月售106单</div>
+                  </section>
+                  <section class="info-brief-right">
+                    <span>30分钟</span>
+                    <span class="shop-distance">443m</span>
+                  </section>
+                </section>
+                <section class="shop-delivery clearfix">
+                  <section class="delivery-left">
+                    <div class="delivery-msg">
+                      <span>¥20起送</span>
+                      <span class="segmentation">/</span>
+                      <span>配送费约¥5</span>
+                    </div>
+                  </section>
+                  <section class="delivery-right">
+                    <span class="delivery-style">硅谷专送</span>
+                  </section>
+                </section>
+              </div>
+            </a>
+          </li>
+          <li class="shop-li border-1px">
+            <a class="shop-li-a clearfix">
+              <div class="shop-left">
+                <img class="shop-img" src="./images/shop/4.jpg" alt="">
+              </div>
+              <div class="shop-right">
+                <section class="shop-info-header clearfix">
+                  <h4 class="shop-title ellipsis">锄禾日当初，汗滴禾下土</h4>
+                  <ul class="info-header-ul">
+                    <li class="supports">保</li>
+                    <li class="supports">准</li>
+                    <li class="supports">票</li>
+                  </ul>
+                </section>
+                <section class="shop-info-brief clearfix">
+                  <section class="info-brief-left">
+                    <div class="star star-24">
+                      <span class="star-item on"></span>
+                      <span class="star-item on"></span>
+                      <span class="star-item on"></span>
+                      <span class="star-item half"></span>
+                      <span class="star-item off"></span>
+                    </div>
+                    <div class="rating-section">3.6</div>
+                    <div class="sales-section">月售106单</div>
+                  </section>
+                  <section class="info-brief-right">
+                    <span>30分钟</span>
+                    <span class="shop-distance">443m</span>
+                  </section>
+                </section>
+                <section class="shop-delivery clearfix">
                   <section class="delivery-left">
                     <div class="delivery-msg">
                       <span>¥20起送</span>
@@ -342,10 +480,9 @@
                 .shop-info-brief
                   width 100%
                   margin-top 18px
-                  margin-bottom 8px
+                  margin-bottom 12px
                   .info-brief-left
                     float left
-                    // color #ff9a0d
                     .star
                       float left
                       font-size 0
@@ -396,8 +533,46 @@
                             bg-image('./images/stars/star48_off')
                     .rating-section
                       float left
+                      font-size 12px
+                      color #ff6000
+                      margin-left 4px
                     .sales-section
                       float left
+                      font-size 12px
+                      color #666
+                      margin-left 10px
                   .info-brief-right
                     float right
+                    font-size 12px
+                    color #666
+                    .shop-distance
+                      &::before
+                        content ''
+                        display inline-block
+                        width 1px
+                        height 6px
+                        margin 0 3px
+                        background #ccc
+                .shop-delivery
+                  width 100%
+                  .delivery-left
+                    float left
+                    .delivery-msg
+                      font-size 12px
+                      color #666
+                      .segmentation
+                        color #ccc
+                        margin 0 4px
+                  .delivery-right
+                    float right
+                    font-size 0 // 解决子元素是inline-block类型标签时会将父元素撑大的bug
+                    .delivery-style
+                      font-size 12px
+                      color #02a774
+                      padding 1px
+                      border 1px solid #02a774
+                      border-radius 2px
+                      display inline-block
+                      transform scale(.8) // 注意:当transform: scale()压缩时,被压缩的元素若是inline类型(display为inline)的行内元素，则压缩不起作用(相当于transform: scale();无效); 但若被压缩的元素是inline-block类型或block类型或浮动的元素，则可以压缩，压缩会起作用; 因此若想使用transform: scale()缩放元素，则该元素不能是inline类型的行内元素，必须是(或者说用display或float转为)inline-block类型的行内块元素或block类型的块元素类型或添加了浮动float的元素才行
+                      transform-origin right top
 </style>
