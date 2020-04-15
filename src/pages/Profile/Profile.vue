b5e7dc52cd9512fdc27064db109d4e5dd35358de
lmy <1751258687@qq.com>
2020-04-15 20:10:54 +0800

    完成Profile组件的静态页面部分

--- src/pages/Profile/Profile.vue
+++ src/pages/Profile/Profile.vue
@@ -52,7 +52,58 @@
         </li>
       </ul>
     </section>
-    <section class="profile-my-order"></section>
+    <section class="profile-menu border-1px">
+      <ul class="menu-items">
+        <!-- 我的订单 -->
+        <li class="menu-item">
+          <a class="item-link">
+            <span class="item-link-icon">
+              <i class="iconfont icon-order"></i>
+            </span>
+            <div class="item-link-title">
+              <span>我的订单</span>
+            </div>
+          </a>
+        </li>
+        <!-- 积分商城 -->
+        <li class="menu-item">
+          <a class="item-link">
+            <span class="item-link-icon">
+              <i class="iconfont icon-jifen"></i>
+            </span>
+            <div class="item-link-title">
+              <span>积分商城</span>
+            </div>
+          </a>
+        </li>
+        <!-- 硅谷外卖会员卡 -->
+        <li class="menu-item">
+          <a class="item-link">
+            <span class="item-link-icon">
+              <i class="iconfont icon-vip"></i>
+            </span>
+            <div class="item-link-title">
+              <span>硅谷外卖会员卡</span>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </section>
+    <section class="profile-menu border-1px">
+      <ul class="menu-items">
+        <!-- 服务中心 -->
+        <li class="menu-item">
+          <a class="item-link">
+            <span class="item-link-icon">
+              <i class="iconfont icon-fuwu"></i>
+            </span>
+            <div class="item-link-title">
+              <span>服务中心</span>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </section>
   </section>
 </template>
 
@@ -142,7 +193,7 @@
           right 15px
           top 40%
           .icon-right-arrow
-            font-size 5px
+            font-size 12px
     .profile-property
       width 100%
       background #fff
@@ -185,7 +236,54 @@
               .property-link-top
                 span
                   color #6ac20b
-    // .profile-my-order
-    //   margin-top 10px
-    //   border-top 1px solid #e4e4e4
+    .profile-menu
+      margin-top 10px
+      background #fff
+      &::before
+        border-bottom 1px solid #e4e4e4
+        border-top 1px solid #e4e4e4
+      .menu-items
+        .menu-item
+          &:last-child
+            .item-link
+              .item-link-title
+                border-bottom 0
+          .item-link
+            display flex
+            align-items center
+            padding-left 15px
+            .item-link-icon
+              width 20px
+              height 20px
+              display flex
+              align-items center
+              >.iconfont
+                margin-left -10px
+                font-size 30px
+              .icon-order
+                color #02a774
+              .icon-jifen
+                color #ff5f3e
+              .icon-vip
+                color #f90
+              .icon-fuwu
+                color #02a774
+            .item-link-title
+              position relative
+              width 100%
+              padding 18px 0
+              border-bottom 1px solid #f1f1f1
+              font-size 16px
+              color #333
+              &::after
+                content ''
+                width 8px
+                height 8px
+                color #bbb
+                border-top 1px solid currentColor
+                border-left 1px solid currentColor
+                position absolute
+                top 50%
+                right 15px
+                transform translate3d(0, -50%, 0) rotate(135deg)
 </style>
