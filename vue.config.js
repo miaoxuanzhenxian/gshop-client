22acc9dcd0bb97b4c2d5294fff86e54283a82638
lmy <1751258687@qq.com>
2020-04-21 21:15:35 +0800

    完善配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+const packjson = require('./package.json')
+process.env.VUE_APP_VERSION = packjson.version
+process.env.VUE_APP_SYSTEMNAME = packjson.name
+
 module.exports = {
   productionSourceMap: false, // 生产环境打包上线时(前)关闭sourcemap(开发中的源文件)
   outputDir: process.env.outputDir // 输出打包后文件的地址，即文件夹名,process.env.outputDir没值时或outputDir: process.env.outputDir这一项没设置时，则默认输出打包后文件的地址，即文件夹名为项目根目录下的dist文件夹
