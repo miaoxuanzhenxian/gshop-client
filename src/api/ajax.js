342707187818868c188163d2238f15499609a362
lmy <1751258687@qq.com>
2020-04-22 19:43:46 +0800

    完成能发ajax请求的函数模块（封装axios,函数的返回值是Promise）

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,9 +1,9 @@
 /* 
   能发ajax请求的函数模块
   封装axios,函数的返回值是Promise
-  1. 对请求体参数进行urlencode处理, 而不使用默认的json方式(后台接口不支持)
-  2. 异步请求成功的数据不是response, 而是response.data
-  3. 统一处理请求异常
+  1. 对post请求体参数进行urlencode处理, 而不使用默认的json方式(后台接口不支持): 请求拦截器
+  2. 异步请求成功的结果数据不是response, 而是response.data: 响应拦截器的成功回调
+  3. 统一处理请求异常、错误：响应拦截器的失败回调
   4. 配置请求超时的时间
   5. 通过请求头携带token数据
   6. 请求loading
@@ -18,6 +18,7 @@ import qs from 'qs'
   .env.[mode]         # 只在指定的模式中被载入
   .env.[mode].local   # 只在指定的模式中被载入，但会被 git 忽略
 */
+// 使用自定义配置创建一个axios实例
 const instance = axios.create({
   baseURL: process.env.VUE_APP_BASE_URL, // 将自动加在 `url` 前面，除非 `url` 是一个绝对 URL。process.env.VUE_APP_BASE_URL为.env.xxx文件中的环境变量VUE_APP_BASE_URL的值。注：.env为一个特定模式准备的环境文件 (例如 .env.production) 将会比一般的环境文件 (例如 .env) 拥有更高的优先级,即若.env.production文件和.env文件中有相同的环境变量，则当加载指定模式加载.env.production文件时，.env文件也会被加载，但.env.production文件中的环境变量优先级更高，其环境变量的值会覆盖.env文件中的相同的环境变量的值
   timeout: 20000 // 配置请求超时的时间
@@ -26,6 +27,7 @@ const instance = axios.create({
 // 添加请求拦截器
 instance.interceptors.request.use(config => {
   const { method, data } = config
+  // 1、对post请求体参数进行urlencode处理, 而不使用默认的json方式(后台接口不支持): 请求拦截器
   if (method.toLowerCase() === 'post' && Object.prototype.toString.call(data) === '[object Object]') {
     config.data = qs.stringify(data)
   }
@@ -34,10 +36,14 @@ instance.interceptors.request.use(config => {
 
 // 添加响应拦截器
 instance.interceptors.response.use(response => {
+  // 2. 异步请求成功的结果数据不是response, 而是response.data: 响应拦截器的成功回调
   return response.data
 }, error => {
-
-  return new Promise(() => { })
+  // 3. 统一处理请求异常、错误：响应拦截器的失败回调
+  alert('请求异常: ' + error.message)
+  // return Promise.reject(error)
+  // 返回一个pending状态(或者叫初始状态或者叫中断状态或者叫空的)的Promise, 中断Promise链
+  return new Promise(() => { }) 
 })
 
 
