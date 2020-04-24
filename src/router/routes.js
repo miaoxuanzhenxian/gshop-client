8826be351dcb33d9610b78e4fc7d0e510a276b66
lmy <1751258687@qq.com>
2020-04-24 22:26:49 +0800

    利用路由元信息(又叫路由元属性或路由元字段)$route.meta来实现控制FooterGuide导航组件的显示与隐藏

--- src/router/routes.js
+++ src/router/routes.js
@@ -15,19 +15,31 @@ const NotFound = () => lazyLoadView(import('../pages/NotFound/NotFound'))
 export default [
   {
     path: '/msite',
-    component: MSite
+    component: MSite,
+    meta: {
+      isShowFooter: true
+    }
   },
   {
     path: '/search',
-    component: Search
+    component: Search,
+    meta: {
+      isShowFooter: true
+    }
   },
   {
     path: '/order',
-    component: Order
+    component: Order,
+    meta: {
+      isShowFooter: true
+    }
   },
   {
     path: '/profile',
-    component: Profile
+    component: Profile,
+    meta: {
+      isShowFooter: true
+    }
   },
   {
     path: '/login',
