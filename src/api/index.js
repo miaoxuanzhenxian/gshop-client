bf1280343eb01e9cbb1c36f3d2a8848e4082f2ce
lmy <1751258687@qq.com>
2020-04-24 18:01:53 +0800

    练习使用代理解决ajax跨域问题

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,11 @@
 import ajax from './ajax'
 
 
+// const BASE = '/api'
+// const BASE2 = '/baidu'
+
 // 1、根据经纬度获取位置详情
+// export const reqAddress = (longitude, latitude) => ajax(BASE + `/position/${latitude},${longitude}`) // 练习使用代理解决ajax跨域问题，另一个后台服务器(http://localhost:4000)
 export const reqAddress = (longitude, latitude) => ajax(`/position/${latitude},${longitude}`)
 
 // 2、获取食品分类列表
@@ -18,3 +22,10 @@ export const reqShops = (longitude, latitude) => ajax('/shops', {
     longitude
   }
 })
+
+// export const reqBaiDuXxx = () => ajax(BASE2 + '/xxx') // 练习使用代理解决ajax跨域问题，另一个后台服务器(http://www.baidu.com)
+
+// // 简单的测试一下接口请求函数
+// reqAddress('116.36867', '40.10038').then(result => {
+//   console.log('result', result)
+// })
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,28 @@ process.env.VUE_APP_VERSION = packjson.version
 process.env.VUE_APP_PROJECTNAME = packjson.name
 
 module.exports = {
+  // 配置开发服务器中的代理
+  // devServer: {
+  //   // proxy: 'http://localhost:4000' // 简单的代理跨域
+  //   proxy: { // 更灵活的、更多的代理跨域控制行为
+  //     // 请求地址以/api开头
+  //     '/api': {
+  //       target: 'http://localhost:4000', // 转发的目标地址
+  //       changeOrigin: true, // 支持跨域
+  //       pathRewrite: { // 重写路径：让代理服务器在转发请求时，对路径进行特定修改
+  //         '^/api': '' // rewrite path，去掉路径中的/api
+  //       }
+  //     },
+  //     // 请求地址以/baidu开头
+  //     '/baidu': {
+  //       target: 'http://www.baidu.com',
+  //       changeOrigin: true,
+  //       pathRewrite: {
+  //         '^/baidu': ''
+  //       }
+  //     }
+  //   }
+  // },
   productionSourceMap: false, // 生产环境打包上线时(前)关闭sourcemap(开发中的源文件)
   outputDir: process.env.outputDir // 输出打包后文件的地址，即文件夹名,process.env.outputDir没值时或outputDir: process.env.outputDir这一项没设置时，则默认输出打包后文件的地址，即文件夹名为项目根目录下的dist文件夹
 }
