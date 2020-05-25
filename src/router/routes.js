64c9e5f551a5ecae7fe3aadb293f4b03d7da9eee
lmy <1751258687@qq.com>
2020-05-26 00:29:46 +0800

    路由懒加载相关注释

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,9 +3,22 @@
 */
 import lazyLoadView from '../utils/lazyLoadView'
 import NotFound from '../pages/NotFound/NotFound'
-import Goods from '../pages/Shop/Goods/Goods'
 
 
+/* 
+路由组件懒加载:
+    1). 在打包时路由组件会被单独打包(代码分割: code split)
+    2). 默认不请求加载路由组件打包文件, 当首次请求需要的路由组件时才请求加载，再次请求相同的路由组件时则不需要了，就会直接用首次已经加载好了的
+1. import动态引入: import(模块路径)
+    结果: 使用import()来引入模块,会对引入模块进行拆分(单独)打包, 即被动态引入的模块会被单独打包(代码分割: code split)   webpack做的
+2. 路由组件配置的是一个函数: 返回包含对应的动态加载的路由组件模块的promise对象的函数
+    函数开始时不执行(开始不请求加载单独打包的路由组件模块代码), 首次请求对应的路径时才会执行, 即当首次请求对应路径需要显示对应的组件界面时, 函数才会执行, 去加载对应的路由组件打包文件，再次请求相同路径的路由组件时则不需要了，就会直接用首次已经加载好了的
+作用: 减小首屏需要加载的js文件大小 ==> 从而使得显示更快，提高了效率，解决首屏加载过慢的问题
+
+实现过程:
+1).对引入模块进行拆分(单独)打包:  使用import()来引入模块
+2).路由组件配置的是一个函数: 返回包含对应的动态加载的路由组件模块的promise对象, 函数初始不会执行, 请求对应的路径时才执行
+*/
 // 路由的懒加载，解决首屏加载过慢的问题
 const MSite = () => lazyLoadView(import('../pages/MSite/MSite'))
 const Search = () => lazyLoadView(import('../pages/Search/Search'))
@@ -13,6 +26,7 @@ const Order = () => lazyLoadView(import('../pages/Order/Order'))
 const Profile = () => lazyLoadView(import('../pages/Profile/Profile'))
 const Login = () => lazyLoadView(import('../pages/Login/Login'))
 const Shop = () => lazyLoadView(import('../pages/Shop/Shop'))
+const Goods = () => lazyLoadView(import('../pages/Shop/Goods/Goods'))
 const Ratings = () => lazyLoadView(import('../pages/Shop/Ratings/Ratings'))
 const Info = () => lazyLoadView(import('../pages/Shop/Info/Info'))
 
