32fdf61a070d575ed1ecc251a919591651410f78
lmy <1751258687@qq.com>
2020-06-10 22:27:52 +0800

    完成Ratings组件和Info组件

--- src/pages/Shop/Info/Info.vue
+++ src/pages/Shop/Info/Info.vue
@@ -1,15 +1,251 @@
 <template>
-  <div>
-    Info
+  <div class="shop-info" ref="shopInfo">
+    <div class="info-content">
+      <section class="section">
+        <h3 class="section-title">配送信息</h3>
+        <div class="delivery">
+          <div>
+            <span class="delivery-icon">{{info.description}}</span>
+            <span>由商家配送提供配送，约{{info.deliveryTime}}分钟送达，距离{{info.distance}}</span>
+          </div>
+          <div class="delivery-money">配送费￥{{info.deliveryPrice}}</div>
+        </div>
+      </section>
+
+      <Split />
+
+      <section class="section">
+        <h3 class="section-title">活动与服务</h3>
+        <div class="activity">
+          <div class="activity-item" v-for="(support, index) in info.supports" :key="index" :class="supportClasses[support.type]">
+            <span class="content-tag">
+              <span class="mini-tag">{{support.name}}</span>
+            </span>
+            <span class="activity-content">{{support.content}}</span>
+          </div>
+        </div>
+      </section>
+      
+      <Split />
+
+      <section class="section">
+        <h3 class="section-title">商家实景</h3>
+        <div class="pic-wrapper" ref="picWrapper">
+          <ul class="pic-list" ref="picsUl">
+            <li class="pic-item" v-for="(pic, index) in info.pics" :key="index">
+              <img width="120" height="90" :src="pic">
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <Split />
+
+      <section class="section">
+        <h3 class="section-title">商家信息</h3>
+        <ul class="detail">
+          <li class="border-1px">
+            <span class="bold">品类</span>
+            <span>{{info.category}}</span>
+          </li>
+          <li class="border-1px">
+            <span class="bold">商家电话</span>
+            <span>{{info.phone}}</span>
+          </li>
+          <li class="border-1px">
+            <span class="bold">地址</span>
+            <span>{{info.address}}</span>
+          </li>
+          <li class="border-1px">
+            <span class="bold">营业时间</span>
+            <span>{{info.workTime}}</span>
+          </li>
+        </ul>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
+  import { mapState } from 'vuex'
+  import BScroll from 'better-scroll'
+
   export default {
-    name: 'Info'
+    name: 'Info',
+
+    data() {
+      return {
+        supportClasses: ['activity-green', 'activity-red', 'activity-orange']
+      }
+    },
+
+    computed: {
+      ...mapState({
+        info: state => state.shop.info
+      })
+    },
+
+    mounted() {
+      if (this.info.name) { // 从其它路由切换过来, 数据已经有了
+        this._initScroll()
+      }
+    },
+
+    watch: {
+      info() { // 在当前路由刷新: 初始显示没有数据, 后面才有了数据
+        this.$nextTick(() => {
+          this._initScroll()
+        })
+      }
+    },
+
+    methods: {
+      _initScroll() {
+        // 整体垂直滑动
+        if (!this.scroll) {
+          this.verticalScroll = new BScroll(this.$refs.shopInfo, {
+            click: true
+          })
+          this.$once('hook:beforeDestroy', () => {
+            this.verticalScroll.destroy()
+          })
+        } else {
+          this.verticalScroll.refresh()
+        }
+
+        // 动态计算指定ul的宽度,解决其内li元素撑不大它的宽度的bug  当然最简单的方式是直接加样式display: inline-block,此处只是练习js而已
+        /* const ul = this.$refs.picsUl
+        const liWidth = 120
+        const space = 6
+        const count = this.info.pics.length
+        ul.style.width = (liWidth + space) * count - space + 'px' */
+
+        // 水平滑动
+        if (!this.levelScroll) {
+          this.levelScroll = new BScroll(this.$refs.picWrapper, {
+            click: true,
+            scrollX: true // 水平滑动
+          })
+          this.$once('hook:beforeDestroy', () => {
+            this.levelScroll.destroy()
+          })
+        } else {
+          this.levelScroll.refresh()
+        }
+      }    
+    }
   }
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus" scoped>
-  
+<style lang="stylus" rel="stylesheet/stylus">
+  .shop-info
+    position absolute
+    top 225px
+    bottom 0
+    left 0
+    width 100%
+    background #fff
+    overflow hidden
+    .section
+      padding 16px 14px 14px
+      font-size 16px
+      background-color #fff
+      color #666
+      border-bottom 1px solid #eee
+      position relative
+      .section-title
+        color #000
+        font-weight 700
+        line-height 16px
+        > .iconfont
+          float right
+          color #ccc
+      .delivery
+        margin-top 16px
+        font-size 13px
+        line-height 18px
+        .delivery-icon
+          width 55px
+          font-size 11px
+          margin-right 10px
+          display inline-block
+          text-align center
+          color #fff
+          background-color #0097ff
+          padding 1px 0
+          border-radius 4px
+        .delivery-money
+          margin-top 5px
+
+      .activity
+        margin-top 16px
+        .activity-item
+          margin-bottom 12px
+          display flex
+          font-size 13px
+          align-items center
+          &.activity-green
+            .content-tag
+              background-color rgb(112, 188, 70)
+          &.activity-red
+            .content-tag
+              background-color rgb(240, 115, 115)
+          &.activity-orange
+            .content-tag
+              background-color rgb(241, 136, 79)
+          .content-tag
+            display inline-block
+            border-radius 2px
+            width 36px
+            height 18px
+            margin-right 10px
+            color #fff
+            font-style normal
+            position relative
+            .mini-tag
+              position absolute
+              left 0
+              top 0
+              right -100%
+              bottom -100%
+              font-size 24px
+              transform scale(.5)
+              transform-origin 0 0
+              display flex
+              align-items center
+              justify-content center
+      .pic-wrapper
+        width 100%
+        overflow hidden
+        white-space nowrap
+        margin-top 16px
+        .pic-list
+          font-size 0
+          display inline-block // 解决其内li元素撑不大它的宽度的bug
+          .pic-item
+            display inline-block
+            margin-right 6px
+            width 120px
+            height 90px
+            &:last-child
+              margin 0
+      .detail
+        margin-bottom -16px
+        > li
+          display flex
+          justify-content space-between
+          align-items center
+          margin-right -10px
+          padding 16px 12px 16px 0
+          line-height 16px
+          &::before
+            height 1px
+            top 100%
+            border-bottom 1px solid #ddd
+          font-size 13px
+          > .bold
+            font-weight 700
+            color #333
+          &:last-child
+            border-none()
 </style>
