d6cff79962ad1e7e21d73d459995cac37055ddb7
lmy <1751258687@qq.com>
2020-06-13 00:26:09 +0800

    使用date-fns实现日期格式化过滤器并进行打包文件大小可视化界面分析与优化(yarn build --report后在打包后的文件夹中会给我们生成一个report.html文件，打开report.html文件之后，就会看到一个可视化的文件占比图，根据哪个文件的占比 比较大，我们就可以对那个文件进行优化操作)

--- src/filters/index.js
+++ src/filters/index.js
@@ -2,10 +2,12 @@
   自定义过滤器模块
  */
 import Vue from 'vue'
-import moment from 'moment'
+import format from 'date-fns/format' // 也能实现过滤日期格式化的功能，并且打包后体积较小，比较好
+// import moment from 'moment' // 打包后体积太大，不太好
 
 
 // 日期格式化过滤器: date-format
-Vue.filter('data-format', (value, formatStr = 'YYYY-MM-DD HH:mm:ss') => {
-  return moment(value).format(formatStr)
+Vue.filter('date-format', (value, formatStr = 'yyyy-MM-dd HH:mm:ss') => {
+  return format(value, formatStr)
+  // return moment(value).format(formatStr)
 })
